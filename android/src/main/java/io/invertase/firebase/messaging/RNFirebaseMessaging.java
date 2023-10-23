1624a51e7611291ed8a1fa7f7f84e8b53825ef20
Brian Ivan Lutaaya <lutbrianivan@gmail.com>
2023-10-23 12:41:47 +0300

    v5.6.0-numida

--- android/src/main/java/io/invertase/firebase/messaging/RNFirebaseMessaging.java
+++ android/src/main/java/io/invertase/firebase/messaging/RNFirebaseMessaging.java
@@ -54,29 +54,38 @@ public class RNFirebaseMessaging extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void getToken(Promise promise) {
-    try {
-      String senderId = FirebaseApp.getInstance().getOptions().getGcmSenderId();
-      String token = FirebaseInstanceId
-        .getInstance()
-        .getToken(senderId, FirebaseMessaging.INSTANCE_ID_SCOPE);
-      promise.resolve(token);
-    } catch (Throwable e) {
-      e.printStackTrace();
-      promise.reject("messaging/fcm-token-error", e.getMessage());
-    }
+  public void getToken(String appName, String senderId, Promise promise) {
+    FirebaseMessaging messagingInstance = FirebaseApp.getInstance(appName).get(FirebaseMessaging.class);
+    Tasks.call(getExecutor(), () -> Tasks.await(messagingInstance.getToken()))
+        .addOnCompleteListener(
+            task -> {
+              if (task.isSuccessful()) {
+                promise.resolve(task.getResult());
+              } else {
+                rejectPromiseWithExceptionMap(promise, task.getException());
+              }
+            }
+        );
   }
 
   @ReactMethod
-  public void deleteToken(Promise promise) {
-    try {
-      String senderId = FirebaseApp.getInstance().getOptions().getGcmSenderId();
-      FirebaseInstanceId.getInstance().deleteToken(senderId, FirebaseMessaging.INSTANCE_ID_SCOPE);
-      promise.resolve(null);
-    } catch (Throwable e) {
-      e.printStackTrace();
-      promise.reject("messaging/fcm-token-error", e.getMessage());
-    }
+  public void deleteToken(String appName, String senderId, Promise promise) {
+    FirebaseMessaging messagingInstance = FirebaseApp.getInstance(appName).get(FirebaseMessaging.class);
+    Tasks.call(
+        getExecutor(),
+        () -> {
+          Tasks.await(FirebaseMessaging.getInstance().deleteToken());
+          Tasks.await(messagingInstance.deleteToken());
+          return null;
+        })
+    .addOnCompleteListener(
+        task -> {
+          if (task.isSuccessful()) {
+            promise.resolve(task.getResult());
+          } else {
+            rejectPromiseWithExceptionMap(promise, task.getException());
+          }
+        });
   }
 
   @ReactMethod
